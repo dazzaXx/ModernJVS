2a5a5094ef8093fd33f17edbd72305cfefc57d58
dazzaXx <48131734+dazzaXx@users.noreply.github.com>
2026-03-09 21:08:33 +0000

    Simplify checksum error logging in modernjvs.c (#149)
    * Simplify checksum error logging in modernjvs.c
    
    * Bump project version to 5.15.16

--- src/modernjvs.c
+++ src/modernjvs.c
@@ -274,15 +274,7 @@ int main(int argc, char **argv)
             switch (processingStatus)
             {
             case JVS_STATUS_ERROR_CHECKSUM:
-                // Check if we're reinitializing (hot-plug event)
-                if (running == 0)
-                {
-                    debug(0, "Warning: Checksum error during controller reinitialization (expected during hot-plug)\n");
-                }
-                else
-                {
-                    debug(0, "Error: A checksum error occurred\n");
-                }
+                debug(0, "Error: A checksum error occurred (Expected if controllers hot-plugged)\n");
                 break;
             case JVS_STATUS_ERROR_WRITE_FAIL:
                 debug(0, "Error: A write failure occurred\n");
