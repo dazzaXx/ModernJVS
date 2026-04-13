86750a9a1343eb3674def2b48d0e5a4353b1e1fb
dazzaXx <48131734+dazzaXx@users.noreply.github.com>
2026-04-13 21:52:01 +0100

    Copy scripts to /home/dietpi
    Copy automation scripts to the dietpi home directory.

--- scripts/Automation_Custom_Script.sh
+++ scripts/Automation_Custom_Script.sh
@@ -4,4 +4,5 @@ sudo chmod +x ModernJVS_Automation_Script.sh
 sudo echo -e "sudo systemctl stop modernjvs\nsudo systemctl stop modernjvs-webui\nsudo systemctl disable modernjvs\nsudo systemctl disable modernjvs-webui\nsudo apt remove -y modernjvs" >> ./Uninstall_ModernJVS.sh
 sudo chmod +x Uninstall_ModernJVS.sh
 sudo ./ModernJVS_Automation_Script.sh
-sudo rm ModernJVS_Automation_Script.sh
+sudo cp ./ModernJVS_Automation_Script.sh /home/dietpi/ModernJVS_Automation_Script.sh
+sudo cp ./Uninstall_ModernJVS.sh /home/dietpi/Uninstall_ModernJVS.sh
