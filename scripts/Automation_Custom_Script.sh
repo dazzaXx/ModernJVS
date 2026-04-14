32294c93a60741c008eae0dbfef375cc8224b835
dazzaXx <48131734+dazzaXx@users.noreply.github.com>
2026-04-14 18:15:27 +0100

    Update Automation_Custom_Script.sh for ModernJVS setup
    Setup automatic bluetooth installation.

--- scripts/Automation_Custom_Script.sh
+++ scripts/Automation_Custom_Script.sh
@@ -1,4 +1,6 @@
 cd ~
+rm /etc/modprobe.d/dietpi-disable_bluetooth.conf
+sed --follow-symlinks -i /^[[:blank:]]*dtoverlay=disable-bt/d /boot/firmware/config.txt
 sudo echo -e "cd ~\nsudo git clone https://github.com/dazzaXx/ModernJVS\ncd ModernJVS\nsudo make install\ncd ..\nsudo rm -r ModernJVS" >> ./ModernJVS_Automation_Script.sh
 sudo chmod +x ModernJVS_Automation_Script.sh
 sudo echo -e "sudo systemctl stop modernjvs\nsudo systemctl stop modernjvs-webui\nsudo systemctl disable modernjvs\nsudo systemctl disable modernjvs-webui\nsudo apt remove -y modernjvs" >> ./Uninstall_ModernJVS.sh
@@ -6,3 +8,4 @@ sudo chmod +x Uninstall_ModernJVS.sh
 sudo ./ModernJVS_Automation_Script.sh
 sudo cp ./ModernJVS_Automation_Script.sh /home/dietpi/ModernJVS_Automation_Script.sh
 sudo cp ./Uninstall_ModernJVS.sh /home/dietpi/Uninstall_ModernJVS.sh
+sudo reboot
