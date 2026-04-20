875805a5930380a663fdc3a91242aef80e68c13a
dazzaXx <48131734+dazzaXx@users.noreply.github.com>
2026-04-20 21:17:52 +0100

    Update git clone command to use specific branch Co-authored-by: dazzaXx <48131734+dazzaXx@users.noreply.github.com>

--- scripts/Automation_Custom_Script.sh
+++ scripts/Automation_Custom_Script.sh
@@ -1,7 +1,7 @@
 cd ~
 rm /etc/modprobe.d/dietpi-disable_bluetooth.conf
 sed --follow-symlinks -i /^[[:blank:]]*dtoverlay=disable-bt/d /boot/firmware/config.txt
-sudo echo -e "cd ~\nsudo git clone https://github.com/dazzaXx/ModernJVS\ncd ModernJVS\nsudo make install\ncd ..\nsudo rm -r ModernJVS" >> ./ModernJVS_Automation_Script.sh
+sudo echo -e "cd ~\nsudo git clone -b v6-code-refactor https://github.com/dazzaXx/ModernJVS\ncd ModernJVS\nsudo make install\ncd ..\nsudo rm -r ModernJVS" >> ./ModernJVS_Automation_Script.sh
 sudo chmod +x ModernJVS_Automation_Script.sh
 sudo echo -e "sudo systemctl stop modernjvs\nsudo systemctl stop modernjvs-webui\nsudo systemctl disable modernjvs\nsudo systemctl disable modernjvs-webui\nsudo apt remove -y modernjvs" >> ./Uninstall_ModernJVS.sh
 sudo chmod +x Uninstall_ModernJVS.sh
